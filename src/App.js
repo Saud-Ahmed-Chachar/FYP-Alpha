6049218d2218023c984e3c796066a2fecb1937b1
Saud Ahmed <113105202+Saud-Ahmed-Chachar@users.noreply.github.com>
2024-04-23 10:53:13 +0500

    app.js update

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ import PortalUniversity from './components/PortalComponents/PortalUniversity';
 import PortalScholarships from './components/PortalComponents/PortalScholarships';
 import PortalScholarshipsDetails from './components/PortalComponents/PortalScholarshipsDetails';
 import PortalAdmissions from './components/PortalComponents/PortalAdmissions';
+import PortalRecommendation from './components/PortalRecommendation';
 const App = () => {
   return (
     <Router>
@@ -47,7 +48,7 @@ const App = () => {
             <Route path="/login" element={<LoginForm />} />
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/signup" element={<SignUpForm />} />
-            <Route path="/recommendation" element={<Recommendation />} />
+            <Route path="/recommendation" element={<PortalRecommendation />} />
             <Route path="/portal/" element={<PortalSideBar />} >
               <Route index element={<UserProfile />}/>
               {/* <Route path='/portal/home' element={<PortalHome/>}/> */}
