486ed919dd63313569624a23a5bcb69d068bc684
ameeralimahar <ameerali.bscssef20@iba-suk.edu.pk>
2024-05-03 02:03:21 +0500

    Updated portal

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -9,8 +9,8 @@ const HomePage = () => {
   return (
     
     <div className="w-full">
-      <div className="bg-gray-100 pt-8">
-                    <div className="container mx-auto flex flex-col items-center">
+      <div className="bg-gray-100 pt-8 pb-8">
+                    <div className="container mx-auto flex flex-col items-center p-4">
                         <div className="w-11/12 sm:w-2/3 lg:flex justify-center items-center flex-col  mb-5 sm:mb-10">
                             <h1 className="text-1xl sm:text-3xl md:text-3xl lg:text-4xl xl:text-5xl text-center text-gray-800 font-black leading-7 md:leading-10">
                                 Welcome to the best
